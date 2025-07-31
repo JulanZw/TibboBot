5f9d88260441deb6de67f00c5d2bbbfc4f9fd78f
Julan <julanzwiggelaar@gmail.com>
2025-07-31 10:11:02 +0200

    Fix for getSuffux function

--- src/utils.ts
+++ src/utils.ts
@@ -216,7 +216,7 @@ export function formatDate(date: Date) {
 export function getDaySuffix(number: number) {
   if (number > 3 && number < 21) return 'th';
   switch (number % 10) {
-    case 1: return '$st';
+    case 1: return 'st';
     case 2: return 'nd';
     case 3: return 'rd';
     default: return 'th';
